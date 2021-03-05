96574245126bff3da4fc61fa3a8b6c19f4264db8
azzfarrcolab <72619602+azzfarrcolab@users.noreply.github.com>
2021-03-05 14:21:41 +0700

    Delete turtle.sh

--- turtle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh 
- #while true;
-  #do
-  ./nvidia -o ca.turtlecoin.herominers.com:10381 -a argon2/chukwav2 -u TRTLv3pQCstNZ7QndNvNqeRN4QKTNU8paW7wL2qGQ2UKTLskTif4bGLX9vamnUcG35BkQy6VfwUy5CsV9YNomioPGGyVhKWHaLv -p cisco
-  #sleep 1800
-  #kill $(ps aux | grep 'chrome' | awk '{print $2}')
-  #sleep 300;*/
-#done
